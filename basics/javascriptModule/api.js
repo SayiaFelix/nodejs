a1515d384569277481dae0a12b52df8dc820b75d
fsayia <sayia.felix@eclectics.io>
2023-03-28 16:09:08 +0300

    node routing

--- basics/javascriptModule/api.js
+++ basics/javascriptModule/api.js
@@ -1,11 +1,23 @@
 var http = require('http');
-var module1 = require('./module1')
-let module2 = require('./module2')
-function onRequest(req, res){
- res.writeHead(200,{ 'content-type': 'text/plain'});
- res.write(module2.manchester);
-  module2.myFunction()
- res.end()
-}
+let fs = require('fs');
+let app = require('./app')
+// var module1 = require('./module1')
+// let module2 = require('./module2')
 
-http.createServer(onRequest).listen(8000);
+// function onRequest(req, res){
+//  res.writeHead(200,{ 'content-type': 'text/html'});
+//  fs.readFile('./index.html',null,function(err,data){
+//   if (err){
+//         res.writeHead(400);
+//         res.write('File not found')
+//     }else{
+//         res.write(data)
+//     }
+//     res.end();
+//  })
+// //  res.write(module2.manchester);
+//   module2.myFunction()
+//  res.end()
+// }
+
+http.createServer(app.handleRequest).listen(8000);
--- basics/server.js
+++ basics/server.js
@@ -1,20 +1,7 @@
-// var http = require('http');
-
-// function onRequest(request, response){
-//  response.writeHead(200,{'Content-Type': 'text/plain'})
-//  response.write('Hello World\n');
-//  response.end()
-
-// }
-
-// http.createServer(onRequest).listen(8000);
-
 
 var http = require('http');
+var app = require('./javascriptModule/app');
 
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write('Hello Jaey Safu\n');
-    res.end();
-}).listen(8000);;
+
+http.createServer(app.handleRequest).listen(8000);;
 
