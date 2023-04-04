f79743e0a6539124f7e55444d56a8d9562670c58
fsayia <sayia.felix@eclectics.io>
2023-04-04 11:18:55 +0300

    adding user login

--- node-restApi-shop/api/routes/user.js
+++ node-restApi-shop/api/routes/user.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken')
 
 const User = require('../model/user')
 
@@ -48,6 +49,55 @@ router.post('/signup', (req, res, next) => {
 
 });
 
+router.post('/login', (req, res, next) => {
+    User.find({ email: req.body.email })
+    .exec()
+    .then(user=>{
+        if (user.length < 1) {
+            return res.status(401).json({
+                error: 'Auth failed'
+            })
+        }
+        bcrypt.compare(req.body.password, user[0].password,(err,result)=>{
+            if(err){
+                console.log(err);
+                return res.status(401).json({
+                    error: 'Auth failed'
+                })
+            }else{
+                if(result){
+                const token = jwt.sign({
+                        email: user[0].email,
+                        userId: user[0]._id
+                    },
+                    'secretjaey',
+                    {
+                        expiresIn: '1h'
+                    });
+                    res.status(200).json({
+                        message: 'Auth success',
+                        user: user[0],
+                        token: token
+                    })
+                }else{
+                    res.status(401).json({
+                        error: 'Auth failed'
+                    })
+                }
+            }
+            // if (user[0].password === req.body.password) {
+            // }
+        })
+    })
+    .catch(err=>{
+        console.log(err);
+        res.status(500).json({
+            error: err
+        })
+    })
+
+});
+
 router.delete('/:userId', (req, res, next) => {
     User.deleteOne({ _id: req.params.userId })
         .exec()
@@ -68,8 +118,5 @@ router.delete('/:userId', (req, res, next) => {
 
 
 })
-
-
-
 
 module.exports = router;
